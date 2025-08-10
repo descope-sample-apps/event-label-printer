d69f598a5fda7fac63ac56d4edda447318ba9f70
Gilad Shriki <shriki@descope.com>
2025-08-09 22:55:09 -0700

    add XL

--- labelPrinterXL.py
+++ labelPrinterXL.py
@@ -1,19 +1,9 @@
 import os
 import time
+
 from dotenv import load_dotenv
 from datetime import datetime
-
-try:
-    import win32printing
-    PRINTING_ENV = True
-except: 
-    print("No win32printing. Running test")
-    PRINTING_ENV = False    
-
-from descope import (
-    AuthException,
-    DescopeClient,
-)
+from descope import ( AuthException, DescopeClient )
 
 LINE_CLEAR = '\x1b[2K' 
 MAX_NAME_LINE = 20
@@ -22,6 +12,14 @@ MAX_TITLE_LINE = 40
 MAX_BALE_HEADER_LINE = 100
 
 print("Setting up...")
+
+try:
+    import win32printing
+    PRINTING_ENV = True
+except: 
+    print("No win32printing. Running without.")
+    PRINTING_ENV = False
+
 load_dotenv()
 management_key = os.getenv("MANAGEMENT_KEY")
 project_id = os.getenv("PROJECT_ID")
@@ -29,17 +27,15 @@ project_id = os.getenv("PROJECT_ID")
 print("Project ID: " + project_id + ". Management Key: " + management_key[:5] + "********" + management_key[-5:])
 
 try:
-    descope_client = DescopeClient(
-        project_id=project_id, management_key=management_key
-    )
+    descope_client = DescopeClient( project_id = project_id, management_key = management_key )
 except Exception as error:
     print("failed to initialize. Error:")
     print(error)
+    exit(1)
 
 def get_print_string(arr, key, max):
     if not key in arr:
         return ""
-
     if (len(arr[key]) > max):
         return arr[key][:max]
     else:
@@ -95,12 +91,10 @@ def updateUser(user):
         print ("Unable to update user's custom attribute.")
         print ("Status Code: " + str(error.status_code))
         print ("Error: " + str(error.error_message))
-
+        exit(1)
 
 def printThis(user):
     print("   Printing " + user["name"])
-    if (not PRINTING_ENV):
-        return user
 
     fontCharSet = "ANSI_CHARSET"
     fontFaceName = "Consolas"
@@ -110,22 +104,32 @@ def printThis(user):
     fontCompany = { "height": 20, "weight": 600, "charSet": fontCharSet, "faceName": fontFaceName }
     fontTitle = { "height": 16, "weight": 400, "charSet": fontCharSet, "faceName": fontFaceName }
 
+    labelHeader = get_print_string(user["customAttributes"],"labelHeader",MAX_BALE_HEADER_LINE)
+    name_lines = get_name_lines(user["name"])                
+    companyName = get_print_string(user["customAttributes"],"companyName",MAX_COMPANY_LINE)
+    title = get_print_string(user["customAttributes"],"title",MAX_TITLE_LINE)
 
-    with win32printing.Printer(
-        printer_name="iDPRT SP410", margin=(0, 0, 5, 0)  # up, right, down, left
-    ) as _printer:
+    if (not PRINTING_ENV):
+        print("Simulated printing...")
+        print("   " + "-" * 35)
+        print("   | " + labelHeader)
+        print("   | " + name_lines[0])
+        print("   | " + name_lines[1])
+        print("   | " + companyName)
+        print("   | " + title)
+        print("   " + "-" * 35)
+        return
+
+    with win32printing.Printer( printer_name="iDPRT SP410", margin=(0, 0, 5, 0) ) as _printer:
         try:
             _printer.start_doc  # start job
             _printer.start_page  # using one label
             
             _printer.text(" ", align="center", font_config=fontGap)  
             
-            labelHeader = get_print_string(user["customAttributes"],"labelHeader",MAX_BALE_HEADER_LINE)
-
             _printer.text(labelHeader, align="center", font_config=fontHeader) 
             _printer.text(" ", align="center", font_config=fontGap)  
 
-            name_lines = get_name_lines(user["name"])                
             _printer.text(name_lines[0], align="center", font_config=fontName)  
             _printer.text(" ", align="center", font_config=fontGap)  
             _printer.text(name_lines[1], align="center", font_config=fontName)  
@@ -134,19 +138,16 @@ def printThis(user):
             _printer.text("\u2500" * 35, align="center")
             _printer.text(" ", align="center", font_config=fontGap)  
 
-            companyName = get_print_string(user["customAttributes"],"companyName",MAX_COMPANY_LINE)
             _printer.text(companyName,align="center",font_config=fontCompany)
 
             _printer.text(" ", align="center", font_config=fontGap)  
 
-            title = get_print_string(user["customAttributes"],"title",MAX_TITLE_LINE)
             _printer.text(title,align="center",font_config=fontTitle)
-            user["customAttributes"]["print"] = False
 
             _printer.end_page
         finally:
             _printer.end_doc
-        return user
+    return
 
 
 def printAlgo():
